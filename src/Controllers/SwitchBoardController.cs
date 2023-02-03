540702ec2b3a698f459904c733dc0e3519081344
Aneesha <anee.guria@gmail.com>
2023-02-03 21:28:36 +0100

    adding controller

---
+++ src/Controllers/SwitchBoardController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwitchBoardApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SwitchBoardController : ControllerBase
+{
+    private readonly ILogger<SwitchBoardController> _logger;
+
+    public SwitchBoardController(ILogger<SwitchBoardController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<string>> GetAllContainerStatus()
+    {
+        return Ok("sdfds");
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateContiner()
+    {
+        return StatusCode(201);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<string>> DeleteContainer()
+    {
+        return Ok("Deleted");
+    }
+}
+
